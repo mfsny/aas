8df3f0133bbbed576354023f8f013dd3f7027f27
slavo <slavo.nagy99@gmail.com>
2016-02-03 11:25:20 +0100

    Update NAStatCounter.scala

--- NAStatCounter.scala
+++ NAStatCounter.scala
@@ -1,30 +1,32 @@
 import org.apache.spark.util.StatCounter
 
-class NAStatCounter extends Serializable{
+class NAStatCounter (d : Double) extends Serializable{
   val stats: StatCounter = new StatCounter()
   var missing: Long = 0
 
   def add(x: Double): NAStatCounter = {
-    //if (java.lang.Double.isNaN(x)){
-    //  missing += 1
-    //} else {
-    //  stats.merge(x)
-    //}
+    if (java.lang.Double.isNaN(x)) {
+      missing += 1
+    } else {
+      stats.merge(x)
+    }
     this
   }
 
   def merge(other: NAStatCounter): NAStatCounter = {
-    //stats.merge(other.stats)
-    //missing += other.missing
+    stats.merge(other.stats)
+    missing += other.missing
     this
   }
 
   override def toString = {
     "stats:" + stats.toString() + "NaN:" + missing
   }
+  
+  add(d)
 }
 
-object NAStatCounter extends Serializable {
+object NAStatCounterFactory extends Serializable {
   def apply(x: Double) = new NAStatCounter().add(x)
 }
 
